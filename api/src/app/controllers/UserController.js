1816ce394fa8c27b0e856eb29ae2499960447dc6
mayconrebordao <r.maycon1122@gmail.com>
2018-04-23 20:18:57 -0300

    Adcionando a rota de atualização de usuários

--- api/src/app/controllers/UserController.js
+++ api/src/app/controllers/UserController.js
@@ -7,60 +7,59 @@ const router = express.Router();
 
 
 // rota para listar todos os usuários
-router.get("/", async (req, res, next) => {
-	const query = User.find().populate('groupTask');
-	query.exec(async (error, docs) => {
-		// console.log(error)
-		if (!error){
-			const users = docs.map( user =>{
-		// console.log(users)
-				return {
-					id: user._id,
-					name: user.name,
-					nickname: user.nickname,
-					email: user.email,
-					groups: user.groupTask.map(  group => {
-						return {
-							id: group._id,
-							name: group.name,
-							description: group.description
-						}
-					})
-					
-				}
-			})
-			res.send({users}).status(200);
-		}
-		else res.send({users: []});
-	})
+router.get("/", async(req, res, next) => {
+    const query = User.find().populate('groupTask');
+    query.exec(async(error, docs) => {
+        // console.log(error)
+        if (!error) {
+            const users = docs.map(user => {
+                // console.log(users)
+                return {
+                    id: user._id,
+                    name: user.name,
+                    nickname: user.nickname,
+                    email: user.email,
+                    groups: user.groupTask.map(group => {
+                        return {
+                            id: group._id,
+                            name: group.name,
+                            description: group.description
+                        }
+                    })
+
+                }
+            })
+            res.send({ users }).status(200);
+        } else res.send({ users: [] });
+    })
 
 
 
 
 
 
-// query.exec(async (error, user) =>{
+    // query.exec(async (error, user) =>{
 
-// 		if(error){
-// 			return  res.status(404).send({
-// 				error: "User not found."
-// 			})
-// 		}
+    // 		if(error){
+    // 			return  res.status(404).send({
+    // 				error: "User not found."
+    // 			})
+    // 		}
 
-// 		console.log(user);
-// 		console.log(password)
-// 		if(!await bcrypt.compare(password, user.password)) {
-// 			return res.status(401).send({
-// 				error: "Password not match."
-// 			})
-// 		}
-// 		user.password = undefined;
-// 		// console.log(user.id);
-// 		// res.send()
-// 		return res.status(200).send({
-// 			token: generateToken({id: user._id })
-// 		})
-// 	})
+    // 		console.log(user);
+    // 		console.log(password)
+    // 		if(!await bcrypt.compare(password, user.password)) {
+    // 			return res.status(401).send({
+    // 				error: "Password not match."
+    // 			})
+    // 		}
+    // 		user.password = undefined;
+    // 		// console.log(user.id);
+    // 		// res.send()
+    // 		return res.status(200).send({
+    // 			token: generateToken({id: user._id })
+    // 		})
+    // 	})
 
 
 
@@ -85,19 +84,38 @@ router.get("/", async (req, res, next) => {
 
     //     })
     // });
-  // res.send({ ok: true });
+    // res.send({ ok: true });
 });
 
-router.post("/", async (req, res, next) => {
-  res.send({ ok: true });
+// router.post("/", async (req, res, next) => {
+//   res.send({ ok: true });
+// });
+
+router.patch("/:userId", async(req, res, next) => {
+
+    const { name, password, email, nickname } = req.body;
+    // console.clear()
+    // console.log({ name, password, email, nickname })
+    if (!name || !password || !email || !nickname) {
+        return res.status(428).send({
+            error: "Email, password, name or nickname is null, but can not be null."
+        })
+    } 
+
+    User.findByIdAndUpdate(req.params.userId, { name, password, email, nickname }, { new:true }, (error, user) => {
+    	// console.log(user)
+    	if(error) 
+    		return res.status(500).send({
+    			error: "Internla error, please try again."
+    		})
+    	else return res.send({ok:true})
+    })
+
+
 });
 
-router.patch("/", async (req, res, next) => {
-  res.send({ ok: true });
-});
-
-router.delete("/", async (req, res, next) => {
-  res.send({ ok: true });
+router.delete("/", async(req, res, next) => {
+    res.send({ ok: true });
 });
 
 module.exports = app => app.use("/users", router);
